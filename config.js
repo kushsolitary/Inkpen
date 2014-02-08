185ec57f92e75033dfc91b0aa4de1c5f5d177b34
Kushagra Agarwal <kushagra666@gmail.com>
2014-02-08 17:57:36 +0530

    updating config

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ var config = {};
 
 
 //Add your twitter and github client ids here...
-config.github_client_id = "abcd";
-config.github_client_secret = "abcd";
-config.twitter_client_id = "abcd";
-config.twitter_client_secret = "abcd";
+config.github_client_id = "c1f98447a7fea5c9672a";
+config.github_client_secret = "09931cd9a38e37915034a01e71fea7110d45d715";
+config.twitter_client_id = "jPL9aoxBSrQMt5NIigiQ";
+config.twitter_client_secret = "BzvPRp1HKePjx2AFeNnbr38IThHWzkalDqPIsr394";
 
 module.exports = config;
---
+++ config_sample.js
@@ -0,0 +1,10 @@
+var config = {};
+
+
+//Add your twitter and github client ids here...
+config.github_client_id = "abcd";
+config.github_client_secret = "abcd";
+config.twitter_client_id = "abcd";
+config.twitter_client_secret = "abcd";
+
+module.exports = config;
